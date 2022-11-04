d1e7e6d170fa60d23a2b49f0890e944dde796198
zst <2356001919@qq.com>
2022-11-04 17:06:00 +0800

    three

--- src/three/TEbasicObject.js
+++ src/three/TEbasicObject.js
@@ -2,8 +2,13 @@ import { Mesh, BoxGeometry, MeshBasicMaterial } from "three";
 
 const arr = [];
 
-const mesh = new Mesh(new BoxGeometry(10, 10, 10), new MeshBasicMaterial({ color: 0x00ff00 }));
+// 地面
+const box = new Mesh(new BoxGeometry(500, 10, 500), new MeshBasicMaterial({ color: "rgb(0,75,75)", roughness: 0 }));
+box.position.y = -5;
 
-arr.push(mesh);
+// 墙面
+const woll = new Mesh(new BoxGeometry(500, 100, 10), new MeshBasicMaterial({ color: "#79bbff" }));
+woll.position.set(0, 50, -245);
 
+arr.push(box, woll);
 export default arr;
--- src/three/index.js
+++ src/three/index.js
@@ -4,61 +4,66 @@ import Stats from "three/examples/jsm/libs/stats.module";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export class TEngine {
-	dom;
-	renderer;
-	scene;
-	camera;
-	constructor(dom) {
-		const H = dom.clientHeight;
-		const W = dom.clientWidth;
-		this.dom = dom;
-		const renderer = new WebGLRenderer({
-			antialias: true,
-		});
-		renderer.setSize(W, H);
-		const scene = new Scene();
-		const camera = new PerspectiveCamera(45, W / H, 0.1, 1000);
-		camera.position.set(20, 20, 20);
-		camera.lookAt(0, 0, 0);
+  dom;
+  renderer;
+  scene;
+  camera;
+  W;
+  H;
+  constructor(dom) {
+    const H = dom.clientHeight;
+    const W = dom.clientWidth;
+    this.dom = dom;
+    const renderer = new WebGLRenderer({
+      antialias: true,
+    });
+    renderer.setSize(W, H);
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(45, W / H, 0.1, 1000);
+    camera.position.set(0, 100, 250);
+    camera.lookAt(0, 0, 0);
 
-		const light = new AmbientLight("#fff");
-		const axeshelper = new AxesHelper(150);
-		const gridhelper = new GridHelper(500, 20, "rgb(200,200,200)", "rgb(100,100,100)");
+    const light = new AmbientLight("#fff");
+    const axeshelper = new AxesHelper(150);
+    const gridhelper = new GridHelper(500, 20, "rgb(200,200,200)", "rgb(100,100,100)");
 
-		scene.add(light);
-		scene.add(axeshelper);
-		scene.add(gridhelper);
+    scene.add(light);
+    scene.add(axeshelper);
+    scene.add(gridhelper);
 
-		const stats = new Stats();
-		stats.domElement.style.position = "absolute";
-		stats.domElement.style.top = "0";
-		stats.domElement.style.right = "5px";
-		stats.domElement.style.left = "unset";
+    const stats = new Stats();
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.top = "0";
+    stats.domElement.style.right = "5px";
+    stats.domElement.style.left = "unset";
 
-		const orbitcontrols = new OrbitControls(camera, renderer.domElement);
-		orbitcontrols.mouseButtons = {
-			LEFT: null,
-			MIDDLE: MOUSE.DOLLY,
-			RIGHT: MOUSE.ROTATE,
-		};
-		function animation() {
-			orbitcontrols.update();
-			renderer.render(scene, camera);
-			stats.update();
-			requestAnimationFrame(animation);
-		}
-		animation();
+    const orbitcontrols = new OrbitControls(camera, renderer.domElement);
+    orbitcontrols.mouseButtons = {
+      LEFT: null,
+      MIDDLE: MOUSE.DOLLY,
+      RIGHT: MOUSE.ROTATE,
+    };
 
-		dom.appendChild(renderer.domElement);
-		dom.appendChild(stats.domElement);
+    function animation() {
+      orbitcontrols.update();
+      renderer.render(scene, camera);
+      stats.update();
+      requestAnimationFrame(animation);
+    }
+    animation();
 
-		this.renderer = renderer;
-		this.scene = scene;
-		this.camera = camera;
-	}
-	addObject(arr) {
-		arr.forEach((element) => {
-			this.scene.add(element);
-		});
-	}
+    dom.appendChild(renderer.domElement);
+    dom.appendChild(stats.domElement);
+
+    this.renderer = renderer;
+    this.scene = scene;
+    this.camera = camera;
+    this.W = W;
+    this.H = H;
+  }
+  addObject(arr) {
+    arr.forEach((element) => {
+      this.scene.add(element);
+    });
+  }
 }
