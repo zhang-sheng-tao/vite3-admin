5496130de3c6c7d43f68a05de82843e6688d77aa
zst <2356001919@qq.com>
2022-11-12 10:39:49 +0800

    21

--- src/api/user.js
+++ src/api/user.js
@@ -1,24 +1,30 @@
 import request from "@/utils/request";
 
 export function login(data) {
-  return request({
-    url: "/api/user/login",
-    method: "post",
-    data,
-  });
+	return request({
+		url: "/api/user/login",
+		method: "post",
+		data,
+	});
 }
 export function info() {
-  return request({
-    url: "/api/user/info",
-  });
+	return request({
+		url: "/api/user/info",
+	});
 }
 export function logout() {
-  return request({
-    url: "/api/user/logout",
-  });
+	return request({
+		url: "/api/user/logout",
+	});
 }
 export function menulist() {
-  return request({
-    url: "/api/user/menulist",
-  });
+	return request({
+		url: "/api/user/menulist",
+	});
+}
+
+export function arr() {
+	return request({
+		url: "/api/arr",
+	});
 }
--- src/view/user/index.vue
+++ src/view/user/index.vue
@@ -2,19 +2,28 @@
   <div class="user">
     <div class="ul" v-scroll.y="scroll">
       <div style="width: 100%">
-        <div v-for="(item, index) in 30" :key="index" class="li">{{ item }}</div>
+        <div v-for="(item, index) in list" :key="index" class="li">{{ item.a }}</div>
       </div>
     </div>
     <el-button type="primary" size="default" v-file.img="file">上传文件</el-button>
   </div>
 </template>
 <script setup>
+import { arr } from '@/api/user'
+const list = ref([])
+
 function scroll() {
   console.log("到底了");
 }
 function file(file) {
   console.log(file);
 }
+
+arr().then(res => {
+  list.value = res.data
+})
+
+
 </script>
 <style lang="scss" scoped>
 .user {
@@ -27,6 +36,7 @@ function file(file) {
     border: 1px solid red;
     overflow: hidden;
     display: inline-block;
+
     .li {
       text-align: center;
       width: 100%;
