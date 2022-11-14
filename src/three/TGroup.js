15c4908de9245741b4c5f0d541c8feedfa65bcde
zst <2356001919@qq.com>
2022-11-14 22:04:17 +0800

    2

--- src/three/TGroup.js
+++ src/three/TGroup.js
@@ -1,30 +1,38 @@
-import { Group, Mesh, MeshBasicMaterial } from "three";
+import { Group, Mesh, MeshBasicMaterial, Sprite, SpriteMaterial } from "three";
 import { PlaneGeometry } from "three/src/geometries/PlaneGeometry.js";
 import { image } from "./TEbasicObject";
 import { framePromis } from "./TLoadModelts";
 
 export const groupPromise = new Promise((reslove, reject) => {
-  const group = new Group();
+	const group = new Group();
 
-  const tips = new Mesh(new PlaneGeometry(10, 8), new MeshBasicMaterial({ color: "#fff" }));
-  tips.position.set(0, -15, 0);
+	const tips = new Mesh(new PlaneGeometry(10, 8), new MeshBasicMaterial({ color: "#fff" }));
+	tips.position.set(0, -15, 0);
+	// 一直看向相机cpu运算
+	//   tips.onBeforeRender = (renderer, scene, cameera) => {
+	//     tips.lookAt(cameera.position);
+	//   };
 
-  // 一直看向相机
-  //   tips.onBeforeRender = (renderer, scene, cameera) => {
-  //     tips.lookAt(cameera.position);
-  //   };
-
-  group.add(tips);
-
-  framePromis()
-    .then((frame) => {
-      group.add(image);
-      group.add(frame);
-      group.position.set(0, 50, -38);
-      group.scale.set(2, 2, 2);
-      reslove(group);
-    })
-    .catch((err) => {
-      reject(err);
-    });
+	// const tips = new Sprite(
+	// 	new SpriteMaterial({
+	// 		color: "#fff",
+	// 		sizeAttenuation: false,
+	// 		depthTest: false,
+	// 		depthWrite: false,
+	// 	})
+	// );
+	// tips.position.set(0, -15, 0);
+	// tips.scale.set(0.08, 0.045, 1);
+	framePromis()
+		.then((frame) => {
+			group.add(tips);
+			group.add(image);
+			group.add(frame);
+			group.position.set(0, 50, -38);
+			group.scale.set(2, 2, 2);
+			reslove(group);
+		})
+		.catch((err) => {
+			reject(err);
+		});
 });
