36406dce43a538749b5642352063f455b7c16ef8
zst <2356001919@qq.com>
2022-11-22 14:04:00 +0800

    dome

---
+++ src/view/three/Dome2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="three" v-observerDom="renderDom" ref="dom"></div>
+</template>
+<script setup>
+import { dome2 } from "@/three/Dome2.js";
+const dom = ref(null);
+const Domes = ref("");
+
+function renderDom(Dom) {
+  Domes.value.renderDom(Dom.width, Dom.height);
+}
+
+onMounted(() => {
+  const DOME = new dome2(dom.value);
+  Domes.value = DOME;
+});
+onBeforeUnmount(() => {
+  cancelAnimationFrame(Domes.value.AnimationId);
+});
+</script>
+<style lang="scss" scoped>
+.three {
+  width: 100%;
+  height: calc(100vh - 50px);
+  position: relative;
+}
+</style>
---
+++ src/view/three/dome1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="three" v-observerDom="renderDom" ref="dom"></div>
+</template>
+<script setup>
+import { dome1 } from "@/three/Dome1.js";
+const dom = ref(null);
+const Domes = ref("");
+
+function renderDom(Dom) {
+  Domes.value.renderDom(Dom.width, Dom.height);
+}
+
+onMounted(() => {
+  const Dome = new dome1(dom.value);
+  Domes.value = Dome;
+});
+onBeforeUnmount(() => {
+  cancelAnimationFrame(Domes.value.AnimationId);
+});
+</script>
+<style lang="scss" scoped>
+.three {
+  width: 100%;
+  height: calc(100vh - 50px);
+  position: relative;
+}
+</style>
